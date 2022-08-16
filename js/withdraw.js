a5327d6495285334684aa1e6ba72161a846c491d
developer-mdshakil <developerdevelopershakil.cmt1819@gmail.com>
2022-08-16 14:03:06 +0600

    modify

--- js/withdraw.js
+++ js/withdraw.js
@@ -3,19 +3,31 @@ document.getElementById('withdraw-btn').addEventListener('click', function(){
     const withdrawAmountString = withdrawField.value;
     const withdrawNewBalance = parseFloat(withdrawAmountString);
 
+    withdrawField.value = '';
+
+    if(isNaN(withdrawNewBalance)){
+        alert('You type Worng input.Please provied valid number');
+        return;
+    }
+
     const withdrawOldBalance = document.getElementById('withdraw-total');
     const withdrawOldBalanceString = withdrawOldBalance.innerText;
     const widtOldBalance = parseFloat(withdrawOldBalanceString);
-    const currentwithdraw = widtOldBalance + withdrawNewBalance;
-    withdrawOldBalance.innerText = currentwithdraw;
+    
 
     const preveousBalance = document.getElementById('total-balance');
     const preveousBalanceString = preveousBalance.innerText;
     const preveousAmount = parseFloat(preveousBalanceString);
+
+    if(withdrawNewBalance > preveousAmount){
+        alert('Poor balance!!!Please set current amount');
+        return;
+    }
+
+    const currentwithdraw = widtOldBalance + withdrawNewBalance;
+    withdrawOldBalance.innerText = currentwithdraw;
+
     const currentBalacne = preveousAmount - withdrawNewBalance;
     preveousBalance.innerText = currentBalacne;
 
-
-
-    withdrawField.value = '';
 })
